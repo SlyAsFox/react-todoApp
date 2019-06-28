07ccda06f01f8478b228bcc92ef335ccb8d5fe1f
SlyAsFox <slyasfox7@gmail.com>
2019-06-28 20:57:28 +0300

    Updates

---
+++ precache-manifest.7edd67e277fb4755f1978d7a6f29bb85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0aee6eb1ff228bb256dc7f145bb188",
+    "url": "/react-todoApp/index.html"
+  },
+  {
+    "revision": "68e0c8312438391fc998",
+    "url": "/react-todoApp/static/css/main.ddf8b45f.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/react-todoApp/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "68e0c8312438391fc998",
+    "url": "/react-todoApp/static/js/main.417dafaf.chunk.js"
+  },
+  {
+    "revision": "2aad321072cff1015558",
+    "url": "/react-todoApp/static/js/runtime~main.15d1b1de.js"
+  }
+]);
