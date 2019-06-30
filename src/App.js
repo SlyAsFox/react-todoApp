3df693dd855578218e7906b5de676a2ca3bf606e
SlyAsFox <slyasfox7@gmail.com>
2019-06-30 09:36:45 +0300

    word-wrap bug was fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoItems from './Components/TodoItems/TodoItems';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(event) {
+    if(this._inputElement.value !== ''){
+      let newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+
+      this.setState((prevState) => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+
+      this._inputElement.value = '';
+    }
+    event.preventDefault();
+  }
+
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function (item) {
+      return (item.key !== key)
+    });
+
+    this.setState({
+      items: filteredItems
+    })
+  }
+
+  render(){
+    return (
+      <div className={ 'App' }>
+        <h1>{ 'ToDo App' }</h1>
+        <div className={ 'header' }>
+          <form onSubmit={ this.addItem }>
+            <input ref={(a) => this._inputElement = a}
+                   placeholder="enter task" type="text">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+        <TodoItems
+            entries={ this.state.items}
+            delete={ this.deleteItem }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
